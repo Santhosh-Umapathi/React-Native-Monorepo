5349efac02c9d2a2a673a90b7b5bf7adf8b4ed63
Santhosh Umapathi <Santhosh.Umapathi@AppyMango.com>
2022-10-29 21:52:05 +0200

    working updates

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import {Button} from '@monorepo/components';
+import {WelcomeScreen} from '@monorepo/screens';
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -17,6 +18,7 @@ const App = () => {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
+      <WelcomeScreen text="wel" />
       <Button onPress={() => console.log('Hi')} title="Package Button" />
     </SafeAreaView>
   );
